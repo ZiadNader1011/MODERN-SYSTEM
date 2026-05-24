00ffed10b3762edaff685d5f47c892938294fcba
ZiadNader1011 <202101425@pua.edu.eg>
2026-05-24 12:55:23 +0300

    ready

--- modern-company-system/import-export/easyflow-logistics-main/src/api/axios.ts
+++ modern-company-system/import-export/easyflow-logistics-main/src/api/axios.ts
@@ -2,7 +2,9 @@ import axios from "axios";
 
 const api = axios.create({
   // ضع رابط الباك اند المرفوع على فيرسيل هنا مباشرة
-  baseURL: import.meta.env.VITE_API_URL || "https://modern-system-flame.vercel.app", 
+  baseURL:
+  import.meta.env.VITE_API_URL ||
+  "https://modern-system-flame.vercel.app/api", 
 });
 
 export default api;
